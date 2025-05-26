ef57a2cfe3a98499471205c6aba9b8d465fe083e
antoniamiddeldorf <antonia.middeldorf@student.htw-berlin.de>
2025-05-26 09:01:27 +0200

    Added a new example sentence

---
+++ src/main/java/de/htw_berlin/fb4/ossd/prose/example/ExampleSentence2.java
@@ -0,0 +1,17 @@
+/* SPDX-FileCopyrightText: 2025 Johannes Kristan
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *  SPDX-License-Identifier: MIT
+ */
+package main.java.de.htw_berlin.fb4.ossd.prose.example;
+
+import de.htw_berlin.fb4.ossd.prose.Sentence;
+
+public class ExampleSentence2 implements Sentence {
+    @Override
+    public String get() {
+        return "Die Vorstellungskraft ist wichtiger als Wissen, denn Wissen ist begrenzt, während die Vorstellungskraft die ganze Welt umfasst, Fortschritte und Innovationen vorantreibt und zur Geburt neuer Ideen inspiriert."; // (Albert Einstein);
+    }
+}
